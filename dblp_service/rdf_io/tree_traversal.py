5761f6d82cac445be8f45376b1c9db8ce27351c1
Adam C. Saunders <saunders@cs.umass.edu>
2023-10-20 13:07:10 -0400

    added documentation

--- dblp_service/rdf_io/tree_traversal.py
+++ dblp_service/rdf_io/tree_traversal.py
@@ -1,3 +1,6 @@
+"""Utilities to convert a tree structure into an ADT representation.
+"""
+
 import typing as t
 
 from bigtree.node.node import Node
@@ -19,6 +22,16 @@ log = create_logger(__file__)
 def get_isA_handler(
     node: Node, handlers: AuthorshipPropertyHandlers
 ) -> t.Optional[t.Callable[[Node, Node], t.Optional[DblpRepr]]]:
+    """
+    Retrieve the handler for 'isA' relationships based on the node's name.
+
+    Args:
+        node (Node): The node to retrieve the handler for.
+        handlers (AuthorshipPropertyHandlers): The handlers for authorship properties.
+
+    Returns:
+        Callable or None: The handler function if it exists and is callable, otherwise None.
+    """
     rel = simplify_urlname(node.node_name)
     handler = f"isA_{rel}"
     if hasattr(handlers, handler) and callable(func := getattr(handlers, handler)):
@@ -28,6 +41,16 @@ def get_isA_handler(
 def get_hasA_handler(
     node: Node, handlers: AuthorshipPropertyHandlers
 ) -> t.Optional[t.Callable[[Node, Node], t.Optional[DblpRepr]]]:
+    """
+    Retrieve the handler for 'hasA' relationships based on the node's name.
+
+    Args:
+        node (Node): The node to retrieve the handler for.
+        handlers (AuthorshipPropertyHandlers): The handlers for authorship properties.
+
+    Returns:
+        Callable or None: The handler function if it exists and is callable, otherwise None.
+    """
     rel = simplify_urlname(node.node_name)
     handler = f"hasA_{rel}"
     if hasattr(handlers, handler) and callable(func := getattr(handlers, handler)):
@@ -35,9 +58,18 @@ def get_hasA_handler(
 
 
 def traverse_authorship_tree(root: Node) -> DblpRepr:
+    """
+    Traverse the authorship tree and return an intermediate encoding.
+
+    Args:
+        root (Node): The root node of the authorship tree.
+
+    Returns:
+        DblpRepr: The representation of the authorship tree.
+    """
     handlers = AuthorshipPropertyHandlers()
 
-    ## First time through, handle the 'isA' properties
+    # First time through, handle the 'isA' properties
     for nsubj, nrel, nobj in iter_subject_triples(root):
         if nrel.node_name != "isA":
             continue
@@ -49,7 +81,7 @@ def traverse_authorship_tree(root: Node) -> DblpRepr:
                 log.info(f"created isA {created}")
                 nsubj.set_attrs(dict(entry=created))
 
-    ## Second time through, handle the 'hasA' properties
+    # Second time through, handle the 'hasA' properties
     for nsubj, nrel, nobj in iter_subject_triples(root):
         if nrel.node_name == "isA":
             continue
@@ -59,7 +91,6 @@ def traverse_authorship_tree(root: Node) -> DblpRepr:
             prior = nsubj.get_attr("entry")
             if prior is None:
                 continue
-            # assert prior is not None
             created = func(nrel, nobj)
             if created:
                 log.info(f"hasA {created}")
@@ -74,6 +105,16 @@ def traverse_authorship_tree(root: Node) -> DblpRepr:
 
 
 def has_prop(pub: Publication, prop: KeyValProp) -> bool:
+    """
+    Check if a publication has a specific property.
+
+    Args:
+        pub (Publication): The publication to check.
+        prop (KeyValProp): The property to look for.
+
+    Returns:
+        bool: True if the publication has the property, otherwise False.
+    """
     def is_match(pub_prop: KeyValProp) -> bool:
         return pub_prop.key == prop.key
 
@@ -82,6 +123,16 @@ def has_prop(pub: Publication, prop: KeyValProp) -> bool:
 
 
 def get_prop(pub: Publication, prop_key: str) -> t.Optional[KeyValProp]:
+    """
+    Retrieve a specific property from a publication.
+
+    Args:
+        pub (Publication): The publication to retrieve the property from.
+        prop_key (str): The key of the property to retrieve.
+
+    Returns:
+        KeyValProp or None: The property if it exists, otherwise None.
+    """
     def is_match(pub_prop: KeyValProp) -> bool:
         return pub_prop.key == prop_key
 
@@ -92,5 +143,13 @@ def get_prop(pub: Publication, prop_key: str) -> t.Optional[KeyValProp]:
 
 
 def authorship_tree_to_repr(root: Node) -> t.List[DblpRepr]:
-    """ """
+    """
+    Convert an authorship tree to an intermediate repr.
+
+    Args:
+        root (Node): The root node of the authorship tree.
+
+    Returns:
+        List[DblpRepr]: A list of reprs for each child of the root node.
+    """
     return [traverse_authorship_tree(subject) for subject in root.children]
